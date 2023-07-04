f7801baa5c4822fec2ab5ba6118c699293ed71a5
Lucas Silva <silva.lucasfeitoza@gmail.com>
2023-07-04 19:13:10 -0300

    Adding environment variables

--- src/services/operacoes.service.js
+++ src/services/operacoes.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: "https://localhost:7185/api/" });
+const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_OPERACOES_API });
 
 const obterColetas = async (pagina, itensPorPagina) =>
     axiosInstance.get(`coletas?pagina=${pagina}&itensPorPagina=${itensPorPagina}`)
--- src/services/solicitacoes.service.js
+++ src/services/solicitacoes.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosInstance = axios.create({ baseURL: "https://localhost:7071/api/" });
+const axiosInstance = axios.create({ baseURL: process.env.REACT_APP_SOLICITACOES_API });
 
 const obterSolicitacoes = async (pagina, itensPorPagina) =>
     axiosInstance.get(`solicitacoes?pagina=${pagina}&itensPorPagina=${itensPorPagina}`)
